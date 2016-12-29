78d68333b59aa0265d9ef3b58ee873122657da28
IvanRistovic <ivo.ristovic@gmail.com>
2016-12-29 22:59:59 +0100

    Created field grid

---
+++ sudoku/Grid.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sudoku
+{
+    class Grid
+    {
+        private int Size = 9;
+        private int CellSize = 30;
+        private Button[,] Field;
+
+
+        public Grid(int size, int cellSize, int startX, int startY, MainForm parent)
+        {
+            Size = size;
+            Field = new Button[Size, Size];
+
+            // Generating field
+            int horizontal = startX;
+            int vertical = startY;
+
+            for (int i = 0; i < Size; i++) {
+                for (int j = 0; j < Size; j++) {
+
+                    // Creating new button
+                    Field[i, j] = new Button()
+                    {
+                        Size = new Size(cellSize, cellSize),
+                        Location = new Point(horizontal, vertical)
+                    };
+
+                    // Adding click event
+                    Field[i, j].Click += OnFieldClick;
+
+                    // Adding control to parent form
+                    parent.Controls.Add(Field[i, j]);
+
+                    // Moving to next column to draw another button
+                    horizontal += cellSize;
+                }
+
+                // Moving to next row
+                horizontal = startX;
+                vertical += cellSize;
+            }
+        }
+
+        private void OnFieldClick(object sender, EventArgs e)
+        {
+            // TODO
+        }
+    }
+}
---
+++ sudoku/MainForm.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace sudoku
+{
+    public partial class MainForm : Form
+    {
+        Grid MainGrid;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            MainGrid = new Grid(9, 30, 10, 10, this);
+        }
+    }
+}
